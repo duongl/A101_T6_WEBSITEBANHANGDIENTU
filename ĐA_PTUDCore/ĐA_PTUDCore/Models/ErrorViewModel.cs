38a3a19d78ef65b5abe54f8871895ae44383fb73
Lê Thành Thái Dương (Hidro) <52492028+duongl@users.noreply.github.com>
2023-12-01 08:37:15 +0700

    fix lỗi famework 6.0

--- ĐA_PTUDCore/ĐA_PTUDCore/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Chatbox.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
